34d73005d7bd446db41a3dd01425120f59d82436
NisheshChakrapani <nishec@uw.edu>
2021-10-31 15:17:42 -0400

    Updating main.py to reflect new buttons for Stage 2

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=["post", "get"])
 def home():
-  if request.form.get("s_signup"):
-    return url_for("s_signup")
-  if request.form.get("s_login"):
-    return url_for("s_login")
+  if request.form.get("s_student_add"):
+    return url_for("s_student_add")
+  if request.form.get("s_professor_add"):
+    return url_for("s_professor_add")
+  if request.form.get("s_delete_entry"):
+    return url_for("s_delete_entry")
   return render_template("index.html")
 
-@app.route("/student_signup", methods=["post", "get"])
-def s_signup():
-  if request.method == "post":
-    user_details = request.form
-  else:
-    return render_template("student_signup.html")
+@app.route("/student_add")
+def s_student_add():
+  return render_template("student_add.html")
 
-@app.route("/student_login")
-def s_login():
-  return render_template("student_login.html")
+@app.route("/professor_add")
+def s_professor_add():
+  return render_template("professor_add.html")
+	
+@app.route("/delete_entry")
+def s_delete_entry():
+  return render_template("delete_entry.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
