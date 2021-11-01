b5592bd15a8140267b48f67718574167a7042d0b
williamwu062 <wwuhoo@gmail.com>
2021-11-01 11:36:01 -0400

    Changed names, etc

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=["post", "get"])
 def home():
-  if request.form.get("s_student_add"):
-    return url_for("s_student_add")
-  if request.form.get("s_professor_add"):
-    return url_for("s_professor_add")
-  if request.form.get("s_delete_entry"):
-    return url_for("s_delete_entry")
+  if request.form.get("addStudent"):
+    return url_for("addStudent")
+  if request.form.get("addProfessor"):
+    return url_for("addProfessor")
+  if request.form.get("deleteEntry"):
+    return url_for("deleteEntry")
   return render_template("index.html")
 
-@app.route("/student_add")
-def s_student_add():
-  return render_template("student_add.html")
+@app.route("/add_student")
+def addStudent():
+  return render_template("addStudent.html")
 
-@app.route("/professor_add")
-def s_professor_add():
-  return render_template("professor_add.html")
+@app.route("/add_professor")
+def addProfessor():
+  return render_template("addProfessor.html")
 	
 @app.route("/delete_entry")
-def s_delete_entry():
-  return render_template("delete_entry.html")
+def deleteEntry():
+  return render_template("deleteEntry.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
